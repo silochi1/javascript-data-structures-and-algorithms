7988ca5fab6d813fc447ebaa873b49e331b84927
Solomon <silochi1@outlook.com>
2025-11-15 19:40:58 -0500

    Added Udemy/YouTube course content to repo

---
+++ DSA_Course/findingNemo.js
@@ -0,0 +1,15 @@
+const nemo = ['nemo'];
+
+function findNemo(array) {
+    let t0 = performance.now();
+    for(let i = 0; i < array.length; i++) {
+        if(array[i] === 'nemo') {
+            console.log('Found Nemo!');
+        }
+    }
+
+    let t1 = performance.now();
+    console.log("Call to findNemo took" + (t1 - t0) + "milliseconds")
+}
+
+findNemo(nemo);
